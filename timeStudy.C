11d80395b8e5c4abe8b9ca5610a728e12cf51306
Gerald <grenier@ipnl.in2p3.fr>
2016-10-04 04:36:54 +0200

    Add greennerve timestudy script

---
+++ timeStudy.C
@@ -0,0 +1,232 @@
+#define timeStudy_cxx
+#include "timeStudy.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+//#include <vector>
+
+//#include "UTIL/CellIDDecoder.h"
+
+float carre(float x) {return x*x;}
+
+struct HitPhase
+{
+  float HitTime;
+  float HitPosition[3];
+  int HitID;
+  float distanceXY(HitPhase &other)
+  { return sqrt( carre(HitPosition[0]-other.HitPosition[0]) + carre(HitPosition[1]-other.HitPosition[1]) );}
+};
+
+
+struct HitPhaseSpace
+{
+  vector<HitPhase*> HitPhaseCollection;
+  vector<int> MuonID;
+  ~HitPhaseSpace()
+  {
+    int n = HitPhaseCollection.size();
+    for(int i = 0; i<n; i++) delete HitPhaseCollection[i];
+  };            
+};
+
+
+struct HitDataRecords
+{
+  float nMissedHit[EffiSize];
+  float nTotalMuonHit;
+  vector<float> minTimeVec;
+
+  HitDataRecords():nTotalMuonHit(0){ for(int i=0; i<EffiSize; i++) nMissedHit[i] = 0; };
+};
+
+
+void MissedHitIdentifier(vector<HitPhaseSpace*>& HPS, vector<HitDataRecords*>& HDRs, int& MultiContriCounter2)
+{
+  for(int iChambre=0; iChambre < 7; iChambre++)
+    {      
+      int nHits = HPS[iChambre]->HitPhaseCollection.size();
+      for(vector<int> ::iterator iter = HPS[iChambre]->MuonID.begin(); iter != HPS[iChambre]->MuonID.end(); ++iter)
+	{
+	  HitPhase* MuonPhase = HPS[iChambre]->HitPhaseCollection[(*iter)];	  
+	  float minT=1e9;
+	  for(int i = 0; i<nHits; i++)
+	    {
+	      if(i!=(*iter))
+		{
+		  HitPhase* AnotherPhase = HPS[iChambre]->HitPhaseCollection[i];
+		  if(AnotherPhase->HitID == MuonPhase->HitID)
+		    {
+		      MultiContriCounter2++;
+		      // cout<<"delta t ="<< (MuonPhase->HitTime)-(AnotherPhase->HitTime)<<";  Dis = "<<AnotherPhase->distanceXY(*MuonPhase)<<endl;  
+		    }
+		  if (AnotherPhase->distanceXY(*MuonPhase) < 25 )
+		    {		     
+		      float t = (MuonPhase->HitTime)-(AnotherPhase->HitTime);
+		      
+		      if (t>0 && t<minT)
+			{
+			  minT = t;			   
+			}
+		    }
+		}
+	    }
+	  // cout << "min T = " << minT << endl;
+	  for(int it = 0; it<EffiSize; it++)
+	    {
+	      float Dtime = it*TimeStep;
+	      if(minT<Dtime)
+		{
+		  HDRs[iChambre]->nMissedHit[it]++;
+		  // cout<<"Dead time = "<<Dtime<<"ns; A hit missed, deta t = "<< minT <<"; chambre: "<<iChambre<<endl;		  
+		}
+	    }
+	  HDRs[iChambre]->minTimeVec.push_back(minT);
+	}
+    }
+}
+
+void timeStudy::Loop()
+{
+  //   In a ROOT session, you can do:
+  //      Root > .L timeStudy.C
+  //      Root > timeStudy t
+  //      Root > t.GetEntry(12); // Fill t data members with entry number 12
+  //      Root > t.Show();       // Show values of entry 12
+  //      Root > t.Show(16);     // Read and show values of entry 16
+  //      Root > t.Loop();       // Loop on all entries
+  //
+
+  //     This is the loop skeleton where:
+  //    jentry is the global entry number in the chain
+  //    ientry is the entry number in the current Tree
+  //  Note that the argument to GetEntry must be:
+  //    jentry for TChain::GetEntry
+  //    ientry for TTree::GetEntry and TBranch::GetEntry
+  //
+  //       To read only selected branches, Insert statements like:
+  // METHOD1:
+  //    fChain->SetBranchStatus("*",0);  // disable all branches
+  //    fChain->SetBranchStatus("branchname",1);  // activate branchname
+  // METHOD2: replace line
+  //    fChain->GetEntry(jentry);       //read all branches
+  //by  b_branchname->GetEntry(ientry); //read only this branch
+  if (fChain == 0) return;
+
+  //   nMuonHitHisto=new TH1I("nmuonhits","Nombre de hits dus aux muons",20,0,20);
+  //muonHitTimeHisto=new TH1F("muonhitstime","temps des hits dus aux muons (ns)",1000,0,1e6);
+
+  // minTimeHisto_P = new vector<TH1F*>(7, (new TH1F("minTime","Minimum time between a hit and a muon hit(ns)", 1000, 0, 1e6)));
+  //vector<TH1F*>& minTimeHisto = *minTimeHisto_P;
+  
+  //UTIL::CellIDDecoder<SimCalorimeterHit> cd("M:3,S-1:3,I:9,J:9,K-1:6");
+   
+  //vector<IMPL::SimCalorimeterHitImpl*> *SDHCAL_Proto_EndCap;
+  // int SameHitCounter = 0;
+
+  Long64_t nentries = fChain->GetEntriesFast();  
+  Long64_t nbytes = 0, nb = 0;
+  int MultiContriCounter = 0;
+  int MultiContriCounter2 = 0;
+ 
+
+  vector<HitDataRecords*>* HDRs_P = new vector<HitDataRecords*>;
+  //(7, new HitDataRecords);
+  vector<HitDataRecords*> & HDRs = *HDRs_P;
+   for(int i=0; i<7; i++) HDRs.push_back(new HitDataRecords);
+     
+  
+  for (Long64_t jentry=0; jentry<nentries;jentry++)
+    {
+      vector<HitPhaseSpace*>* HPS_P = new vector<HitPhaseSpace*>;
+      //(7, new HitPhaseSpace);
+      vector<HitPhaseSpace*>& HPS = *HPS_P;
+      for(int i=0; i<7; i++) HPS.push_back(new HitPhaseSpace);
+      
+      
+       
+      // int nMuonHit=0;
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;      
+      int nHits=SDHCAL_Proto_EndCap->size();
+      std::vector<IMPL::SimCalorimeterHitImpl*> &hitvec=*SDHCAL_Proto_EndCap;
+      for (int ihit=0; ihit<nHits; ++ihit)
+	{
+	  IMPL::SimCalorimeterHitImpl* hit=hitvec[ihit];
+	  int iChambre=( ( (hit->getCellID0())>>24)&63);
+	  //cout << ( ( (hit->getCellID0())>>24)&63) << " : ";
+	  //cout << "chambre " << cd(hit)["K-1"] << " a z=" << hit->getPosition()[2] << endl;
+	  // cout << "chambre " << numeroChambre << " a z=" << hit->getPosition()[2] << endl;
+	  //cout << "test " << hit->_vec.size() << endl;
+	  //cout << "PDG : ";
+	  int ct = 0, cm=0;
+	  for (IMPL::MCParticleContVec::iterator it=hit->_vec.begin(); it!=hit->_vec.end(); ++it)
+	    {
+	      ct++;
+	      HitPhase* aPointer = new HitPhase;
+	      aPointer->HitTime = (*it)->Time;
+	      aPointer->HitPosition[0] = (*it)->StepPosition[0];
+	      aPointer->HitPosition[1] = (*it)->StepPosition[1];
+	      aPointer->HitPosition[2] = (*it)->StepPosition[2];
+	      aPointer->HitID = ihit;
+	      HPS[iChambre]->HitPhaseCollection.push_back(aPointer);
+	      if ((*it)->PDG == -13)
+		{
+		  cm++;
+		  HPS[iChambre]->MuonID.push_back(HPS[iChambre]->HitPhaseCollection.size()-1);
+		  // cout<<"muon hit time: "<<(*it)->Time<<"at chambre "<<iChambre<<endl;
+		  HDRs[iChambre]->nTotalMuonHit++;
+		  // ++nMuonHit;
+		  //		       muonHitTimeHisto->Fill( (*it)->Time );
+		  //		       muonHitTime = (*it)->Time;
+		  //		       muonHitPosition = (*it)->StepPosition;		      
+		}		  
+	    }
+	  if( cm>0 && ct>1 )
+	    {
+	      //cout<<"ct = "<<ct<<";   cm = "<<cm<<endl;
+	      MultiContriCounter+=(ct-cm);
+	    }
+	  //cout << endl;
+	    
+	}
+      MissedHitIdentifier(HPS,  HDRs, MultiContriCounter2);
+
+      for(int i =0; i<7 ; i++) 	delete HPS[i];
+
+      delete HPS_P;
+      
+      
+      //nMuonHitHisto->Fill(nMuonHit);
+      //cout << " nombre de hits muons " << nMuonHit << endl;
+    }
+  for(int iChambre = 0; iChambre<7; iChambre++)     
+    {
+      cout<<"Number of total muon hits(including those missed) for Chambre "<<iChambre<<" is: "<<HDRs[iChambre]->nTotalMuonHit<<endl;
+      //cout<<"Number of missed hits for Chambre "<<iChambre<<" is: "<<HDRs[iChambre]->nMissedHit<<endl;
+      //cout<<"Those missed hits are at time: "<<endl;
+       for(vector<float>:: iterator iter = HDRs[iChambre]->minTimeVec.begin(); iter != HDRs[iChambre]->minTimeVec.end(); iter++)
+	{
+	  minTimeHisto[iChambre]->Fill(  (*iter)  );
+	  // if((*iter)<50) cout<< (*iter)<<endl;
+	}
+      // float t1 = float(nMissedHit);
+      // float t2 = float(MuonID->size());
+      //float fT = HDRs[iChambre]->nTotalMuonHit;
+       // float fM = HDRs[iChambre]->nMissedHit;
+       // cout<<"L'effecacité de la chambre "<<iChambre<<" est: "<<(fT-fM)/fT<<endl;
+       float nT = HDRs[iChambre]->nTotalMuonHit;
+       for(int i=0; i<EffiSize; i++)
+	 {
+	   float nM = HDRs[iChambre]->nMissedHit[i];	   
+	   EffiByTime[iChambre][i] = (nT-nM)/nT;
+	 }
+    }
+  cout<<"multi-contributors number 1(excluded muons) "<<MultiContriCounter<<endl;
+  cout<<"multi-contributors number 2(excluded muons) "<<MultiContriCounter2<<endl;
+  for(int i =0; i<7 ; i++) delete HDRs[i];
+  delete HDRs_P;
+    
+}
---
+++ timeStudy.h
@@ -0,0 +1,164 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Wed Jun 29 17:23:17 2016 by ROOT version 5.34/14
+// from TTree LCIO/lcio event data tree
+// found on file: lcioTree.root
+//////////////////////////////////////////////////////////
+
+#ifndef timeStudy_h
+#define timeStudy_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TH1I.h>
+
+// Header file for the classes stored in the TTree if any.
+#include <vector>
+#include <string>
+
+//LCIO heqder file
+#include "IMPL/SimCalorimeterHitImpl.h"
+
+
+const static int EffiSize = 1000;
+const static float TimeStep = 2.5;
+//const static float TempsMort = 5;
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+
+
+class timeStudy {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   vector<IMPL::SimCalorimeterHitImpl*> *SDHCAL_Proto_EndCap;
+
+   // List of branches
+   TBranch        *b_SDHCAL_Proto_EndCap;   //!
+
+   timeStudy(TTree *tree=0);
+   virtual ~timeStudy();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+
+   //   TH1I *nMuonHitHisto;
+   //  TH1F *muonHitTimeHisto;
+   TH1F* minTimeHisto[7];
+   // , minTimeHisto(7, (new TH1F("minTime","Minimum time between a hit and a muon hit(ns)", 1000, 0, 1e6)))
+   float* EffiByTime[7];
+   float  DeadTime[EffiSize];
+   //   float  eff;
+};
+
+#endif
+
+#ifdef timeStudy_cxx
+timeStudy::timeStudy(TTree *tree) : fChain(0)
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("lcioTree.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("lcioTree.root");
+      }
+      f->GetObject("LCIO",tree);
+
+   }
+   Init(tree);
+
+   
+   for(Int_t i=0; i<7; i++)
+     {
+       TString HisName;
+       HisName.Form("%d",i);
+       HisName = "TimeHisot@Chambre"+HisName;
+       minTimeHisto[i] = new TH1F(HisName,"Minimum time between a hit and a muon hit(ns)", 1000, 0, 1e6);
+     }
+   for(int i=0; i<EffiSize; i++) DeadTime[i] = TimeStep*((float)i);
+   for(int i=0; i<7; i++) EffiByTime[i] = new float[EffiSize];
+   //   eff = 0;  
+}
+
+timeStudy::~timeStudy()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+   for(int j=0; j<7; j++){ delete[] EffiByTime[j];}
+}
+
+Int_t timeStudy::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t timeStudy::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void timeStudy::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   SDHCAL_Proto_EndCap = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("SDHCAL_Proto_EndCap", &SDHCAL_Proto_EndCap, &b_SDHCAL_Proto_EndCap);
+   Notify();
+}
+
+Bool_t timeStudy::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void timeStudy::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t timeStudy::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef timeStudy_cxx
