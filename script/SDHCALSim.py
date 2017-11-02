4e7c1df2c649441825036574260798a07b250cd9
kohntarkosz <gari.guillaume@gmail.com>
2017-11-02 17:15:20 +0100

    remove useless scripts

--- script/SDHCALSim.py
+++ script/SDHCALSim.py
@@ -2,13 +2,11 @@
 
 import os
 
-class Params :
+class Particle :
 	def __init__(self) :
-		self.particle = "mu-"
-		self.physicsList = "FTFP_BERT"
+		self.particleName = "mu-"
 
-		self.nEvent = 100
-		self.seed = 0
+		self.time = 0
 
 		self.positionOption = "fixed"       #fixed uniform gaus cosmic
 		self.positionX = 0                  #0 is calorimeter center
@@ -20,7 +18,7 @@ class Params :
 		self.momentumOption = "fixed"       #fixed gaus
 		self.momentumPhi = 0                #
 		self.momentumTheta = 0              #
-		self.sigmaMomentum = 0.0            #if gaus
+		self.sigmaMomentum = 0              #if gaus
 
 		self.energyDistribution = "fixed"   #fixed gaus uniform forNN
 		self.energy = 50                    #if fixed or gaus
@@ -29,43 +27,53 @@ class Params :
 		self.maxEnergy = 120                #if uniform or forNN
 
 
+class Params :
+	def __init__(self) :
+		self.physicsList = "FTFP_BERT"
+
+		self.nEvent = 100
+		self.seed = 0
+		self.particleList = []
+		self.rpcType = "normal"
+		self.outputFileName = "output"
+
+
+
 def launch(a) :
 
 	pid = os.getpid()
-	macFileName = str(pid) + '.mac'
+	xmlFileName = str(pid) + '.xml'
 
-	macFileContent = '''
-/GunParameter/Particle ''' + a.particle + '''
+	xmlFileContent = '''
+	<config>
+		<physicsList>'''+ a.physicsList +'''</physicsList>
+		<nEvent>'''+ str(a.nEvent) +'''</nEvent>
+		<seed>'''+ str(a.seed) +'''</seed>
+		<rpcType>'''+ a.rpcType +'''</rpcType>
+		<outputFileName>'''+ a.outputFileName +'''</outputFileName>
+		<guns>'''
 
-/GunParameter/PositionOption ''' + a.positionOption + '''
-/GunParameter/PositionX ''' + str(a.positionX) + '''
-/GunParameter/PositionY ''' + str(a.positionY) + '''
-/GunParameter/PositionZ ''' + str(a.positionZ) + '''
-/GunParameter/UniformDeltaPosition ''' + str(a.uniformDeltaPos) + '''
-/GunParameter/GausSigmaPosition ''' + str(a.sigmaPos) + '''
+	for particle in a.particleList :
+		xmlFileContent = xmlFileContent + '''
+			<particle>
+					<pdgID>''' + particle.particleName + '''</pdgID>
+					<time>''' + str(particle.time) + '''</time>
+					<energy type="''' + particle.energyDistribution+ '''" sigma="''' + str(particle.sigmaEnergy) + '''" min="''' + str(particle.minEnergy) + '''" max="''' + str(particle.maxEnergy) + '''">''' + str(particle.energy) + '''</energy>
+					<position type="''' + particle.positionOption + '''" delta="''' + str(particle.uniformDeltaPos) + '''" sigma="''' + str(particle.sigmaPos) + '''">''' + str(particle.positionX) + ''' ''' + str(particle.positionY) + ''' ''' + str(particle.positionZ) + '''</position> 
+					<momentum type="''' + particle.momentumOption + '''" sigma="''' + str(particle.sigmaMomentum) + '''">''' + str(particle.momentumPhi) + ''' ''' + str(particle.momentumTheta) + '''</momentum>
+			</particle>'''
 
-/GunParameter/MomentumOption ''' + a.momentumOption + '''
-/GunParameter/MomentumSigma ''' + str(a.sigmaMomentum) + '''
-/GunParameter/MomentumPhi ''' + str(a.momentumPhi) + '''
-/GunParameter/MomentumTheta ''' + str(a.momentumTheta) + '''
+	xmlFileContent = xmlFileContent + '''
+		</guns>
+	</config>'''
 
-/GunParameter/EnergyDistribution ''' + a.energyDistribution + '''
-/GunParameter/Energy ''' + str(a.energy) + '''
-/GunParameter/SigmaEnergy ''' + str(a.sigmaEnergy) + '''
-/GunParameter/MinEnergy ''' + str(a.minEnergy) + '''
-/GunParameter/MaxEnergy ''' + str(a.maxEnergy) + '''
-
-/run/beamOn ''' + str(a.nEvent) + '''
-	'''
-
-	macFile = open(macFileName , 'w')
-	macFile.write(macFileContent)
-	macFile.close()
+	xmlFile = open(xmlFileName , 'w')
+	xmlFile.write(xmlFileContent)
+	xmlFile.close()
 
 
 	simuExe = os.environ["SIMEXE"]
 
-	os.system(simuExe + ' ' + macFileName + ' ' + str(a.seed) + ' ' + str(a.physicsList))
+	os.system(simuExe + ' ' + xmlFileName)
 
-	os.system('rm ' + macFileName)
-
+	os.system('rm ' + xmlFileName)
