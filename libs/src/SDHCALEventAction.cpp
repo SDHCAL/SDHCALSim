c195a580b7ae3a00624deb25c541f8ac1ebd8583
flagarde <lagarde@sjtu.edu.cn>
2020-04-14 13:18:25 +0800

    Cosmetics

--- libs/src/SDHCALEventAction.cpp
+++ libs/src/SDHCALEventAction.cpp
@@ -13,7 +13,6 @@ SDHCALEventAction::SDHCALEventAction(SDHCALRunAction* runAction,SDHCALSteppingAc
 {
 }
 
-
 void SDHCALEventAction::BeginOfEventAction(const G4Event* event)
 {
   beginClock = clock() ;
@@ -25,125 +24,116 @@ void SDHCALEventAction::BeginOfEventAction(const G4Event* event)
 void SDHCALEventAction::EndOfEventAction(const G4Event* event)
 {
   G4AutoLock	l(&aMutex);
-	G4cout << "-------------------------------------------------------" << G4endl ;
-	G4cout << "Event " << event->GetEventID() << G4endl ;
-  SDHCALLcioWriter* lcioWriter = m_RunAction->getLcioWriter() ;
-  lcioWriter->clear() ;
-  lcioWriter->createLCEvent(event) ;
-	SDHCALRootWriter* rootWriter = m_RunAction->getRootWriter() ;
-
-	std::vector<SDHCALHit*> hits ;
+  G4cout<<"-------------------------------------------------------"<<G4endl;
+  G4cout<<"Event "<<event->GetEventID()<<G4endl;
+  SDHCALLcioWriter* lcioWriter = m_RunAction->getLcioWriter();
+  lcioWriter->clear();
+  lcioWriter->createLCEvent(event);
+  SDHCALRootWriter* rootWriter = m_RunAction->getRootWriter() ;
+  
+  std::vector<SDHCALHit*> hits;
    //std::cout<< event->GetHCofThisEvent()->GetNumberOfCollections() <<std::endl;
-	for (int i = 0 ; i < event->GetHCofThisEvent()->GetNumberOfCollections() ; i++ )
-	{
-		SDHCALHitCollection* hCol = dynamic_cast<SDHCALHitCollection*>( event->GetHCofThisEvent()->GetHC(i) ) ;
-		std::vector<SDHCALHit*> hitVec = *( hCol->GetVector() ) ;
-		hits.insert(hits.end() , hitVec.begin() , hitVec.end() ) ;
-    
-	}
+ /* for (int i = 0 ; i < event->GetHCofThisEvent()->GetNumberOfCollections() ; i++ )
+  {
+    SDHCALHitCollection* hCol = dynamic_cast<SDHCALHitCollection*>( event->GetHCofThisEvent()->GetHC(i) ) ;
+    std::vector<SDHCALHit*> hitVec = *( hCol->GetVector() ) ;
+    hits.insert(hits.end() , hitVec.begin() , hitVec.end() ) ;
+  }*/
 
-	for(std::size_t i=0;i!=hits.size();++i)
+  for(std::size_t i=0;i!=hits.size();++i)
   {
     hits[i]->setPrimaryID(m_TrackingAction->getPrimaryParent(hits[i]->getTrackID()));
   }
-	
-	lcioWriter->createPrimaryParticles(event) ;
 
-	lcioWriter->createSimCalorimeterHits(hits) ;
+  lcioWriter->createPrimaryParticles(event) ;
 
-	std::shared_ptr<SDHCALGun> gun = m_PrimaryGeneratorAction->getGunVec().at(0) ;
+  lcioWriter->createSimCalorimeterHits(hits) ;
 
-	G4double primaryEnergy = gun->GetParticleEnergy()/CLHEP::GeV ;
-	G4ThreeVector primaryMom = gun->GetParticleMomentumDirection() ;
-	G4ThreeVector primaryPos = gun->GetParticlePosition() ;
-	G4int particleID = gun->GetParticleDefinition()->GetPDGEncoding() ;
+  std::shared_ptr<SDHCALGun> gun = m_PrimaryGeneratorAction->getGunVec().at(0) ;
 
-	lcioWriter->setValue("ParticleID" , particleID) ;
-	lcioWriter->setValue("ParticleMomentum" , primaryMom) ;
-	lcioWriter->setValue("ParticlePosition" , primaryPos) ;
-	lcioWriter->setValue("ParticleEnergy" , primaryEnergy) ;
+  G4double primaryEnergy = gun->GetParticleEnergy()/CLHEP::GeV ;
+  G4ThreeVector primaryMom = gun->GetParticleMomentumDirection() ;
+  G4ThreeVector primaryPos = gun->GetParticlePosition() ;
+  G4int particleID = gun->GetParticleDefinition()->GetPDGEncoding() ;
 
-
-	m_PrimaryGeneratorAction->print() ;
-	G4cout << G4endl ;
-
-	m_SteppingAction->processSteps() ;
-
-	G4double depositedEnergy = m_SteppingAction->getDepositedEnergy()/CLHEP::GeV ;
-	G4double leakedEnergy = m_SteppingAction->getLeakedEnergy()/CLHEP::GeV ;
-	G4double emFraction = m_SteppingAction->getEMFraction() ;
-	G4double depositedEnergyNeutron = m_SteppingAction->getDepositedEnergyPerParticleType()[2112]/CLHEP::GeV ;
-
-	G4cout << "Deposited energy : " << depositedEnergy << " GeV" << G4endl ;
-	G4cout << "Leaked energy : " << leakedEnergy << " GeV" << G4endl ;
-	G4cout << "EM Fraction : " << 100*emFraction << " %" << G4endl ;
-	G4cout << G4endl ;
-
-	lcioWriter->setValue("DepositedEnergy" , depositedEnergy ) ;
-	lcioWriter->setValue("DepositedEnergyNeutron" , depositedEnergyNeutron ) ;
-	lcioWriter->setValue("LeakedEnergy" , leakedEnergy ) ;
-	lcioWriter->setValue("EMFraction" , emFraction ) ;
-
-	const auto& nParticlesPerIDMap = m_StackingAction->getNumberOfParticlesPerID() ;
-	G4int nNeutrons = 0 ;
-	G4int nPi0 = 0 ;
-
-	if ( nParticlesPerIDMap.count(2112) )
-		nNeutrons = nParticlesPerIDMap.at(2112) ;
-	if ( nParticlesPerIDMap.count(111) )
-		nPi0 = nParticlesPerIDMap.at(111) ;
-
-	lcioWriter->setValue("nNeutrons" , nNeutrons ) ;
-	lcioWriter->setValue("nPi0" , nPi0 ) ;
+  lcioWriter->setValue("ParticleID" , particleID) ;
+  lcioWriter->setValue("ParticleMomentum" , primaryMom) ;
+  lcioWriter->setValue("ParticlePosition" , primaryPos) ;
+  lcioWriter->setValue("ParticleEnergy" , primaryEnergy) ;
 
 
+  m_PrimaryGeneratorAction->print() ;
+  G4cout << G4endl ;
 
-	lcioWriter->writeLCEvent() ;
+  m_SteppingAction->processSteps() ;
 
+  G4double depositedEnergy = m_SteppingAction->getDepositedEnergy()/CLHEP::GeV ;
+  G4double leakedEnergy = m_SteppingAction->getLeakedEnergy()/CLHEP::GeV ;
+  G4double emFraction = m_SteppingAction->getEMFraction() ;
+  G4double depositedEnergyNeutron = m_SteppingAction->getDepositedEnergyPerParticleType()[2112]/CLHEP::GeV ;
 
+  G4cout << "Deposited energy : " << depositedEnergy << " GeV" << G4endl ;
+  G4cout << "Leaked energy : " << leakedEnergy << " GeV" << G4endl ;
+  G4cout << "EM Fraction : " << 100*emFraction << " %" << G4endl ;
+  G4cout << G4endl ;
 
-	int nRealHits = 0 ;
-	for ( std::vector<SDHCALHit*>::const_iterator it = hits.begin() ; it != hits.end() ; ++it )
-	{
-		if ( ( (*it)->getEndPos() - (*it)->getBeginPos() ).mag() > 0 )
-			nRealHits++ ;
-	}
+  lcioWriter->setValue("DepositedEnergy" , depositedEnergy ) ;
+  lcioWriter->setValue("DepositedEnergyNeutron" , depositedEnergyNeutron ) ;
+  lcioWriter->setValue("LeakedEnergy" , leakedEnergy ) ;
+  lcioWriter->setValue("EMFraction" , emFraction ) ;
 
-	//processing time
-	double timeOfThisEvent = 1.0*( clock() - beginClock )/CLOCKS_PER_SEC ;
-	G4cout << "computing time : " << timeOfThisEvent << G4endl ;
+  const auto& nParticlesPerIDMap = m_StackingAction->getNumberOfParticlesPerID() ;
+  G4int nNeutrons = 0 ;
+  G4int nPi0 = 0 ;
 
-	averageTime = (averageTime*nEventsProcessed + timeOfThisEvent)/(nEventsProcessed+1) ;
-	nEventsProcessed++ ;
-	G4cout << "average time : " << averageTime << G4endl ;
+  if(nParticlesPerIDMap.count(2112)) nNeutrons = nParticlesPerIDMap.at(2112) ;
+  if(nParticlesPerIDMap.count(111)) nPi0 = nParticlesPerIDMap.at(111) ;
 
+  lcioWriter->setValue("nNeutrons" , nNeutrons ) ;
+  lcioWriter->setValue("nPi0" , nPi0 ) ;
+  lcioWriter->writeLCEvent() ;
 
-	rootWriter->setEventNumber( event->GetEventID() ) ;
-	rootWriter->setNHit( nRealHits ) ;
-	rootWriter->setPrimaryID( particleID ) ;
-	rootWriter->setPrimaryEnergy( primaryEnergy ) ;
-	rootWriter->setPrimaryPos( primaryPos ) ;
-	rootWriter->setPrimaryMom( primaryMom ) ;
-	rootWriter->setDepositedEnergy( depositedEnergy ) ;
-	rootWriter->setDepositedEnergyNeutron( depositedEnergyNeutron ) ;
-	rootWriter->setNNeutrons( nNeutrons ) ;
-	rootWriter->setNPi0( nPi0 ) ;
-	rootWriter->setLeakedEnergy( leakedEnergy ) ;
-	rootWriter->setEmFraction( emFraction ) ;
-	rootWriter->setComputingTime( timeOfThisEvent ) ;
+  int nRealHits{0};
+  for ( std::vector<SDHCALHit*>::const_iterator it = hits.begin() ; it != hits.end() ; ++it )
+  {
+    if ( ( (*it)->getEndPos() - (*it)->getBeginPos() ).mag() > 0 )
+    nRealHits++ ;
+  }
 
-	std::vector<double> stepCosAngle ;
-	std::vector<double> stepLength ;
-	std::vector<double> stepTime ;
-	for ( std::vector<SDHCALHit*>::const_iterator it = hits.begin() ; it != hits.end() ; ++it )
-	{
-		stepCosAngle.push_back( std::cos( (*it)->getDeltaPos().angle( (*it)->getRPC()->rpcToGlobalCoordTransform( G4ThreeVector(0,0,1) ) ) ) ) ;
-		stepLength.push_back( (*it)->getTrueLength() ) ;
-		stepTime.push_back( (*it)->getTime() ) ;
-	}
-	
-	rootWriter->setStepCosAngle( stepCosAngle ) ;
-	rootWriter->setStepLength( stepLength ) ;
-	rootWriter->setStepTime( stepTime ) ;
-	rootWriter->fillTree() ;
+  //processing time
+  double timeOfThisEvent = 1.0*( clock() - beginClock )/CLOCKS_PER_SEC ;
+  G4cout << "computing time : " << timeOfThisEvent << G4endl ;
+
+  averageTime = (averageTime*nEventsProcessed + timeOfThisEvent)/(nEventsProcessed+1) ;
+  nEventsProcessed++ ;
+  G4cout << "average time : " << averageTime << G4endl ;
+
+  rootWriter->setEventNumber( event->GetEventID() ) ;
+  rootWriter->setNHit( nRealHits ) ;
+  rootWriter->setPrimaryID( particleID ) ;
+  rootWriter->setPrimaryEnergy( primaryEnergy ) ;
+  rootWriter->setPrimaryPos( primaryPos ) ;
+  rootWriter->setPrimaryMom( primaryMom ) ;
+  rootWriter->setDepositedEnergy( depositedEnergy ) ;
+  rootWriter->setDepositedEnergyNeutron( depositedEnergyNeutron ) ;
+  rootWriter->setNNeutrons( nNeutrons ) ;
+  rootWriter->setNPi0( nPi0 ) ;
+  rootWriter->setLeakedEnergy( leakedEnergy ) ;
+  rootWriter->setEmFraction( emFraction ) ;
+  rootWriter->setComputingTime( timeOfThisEvent ) ;
+
+  std::vector<double> stepCosAngle ;
+  std::vector<double> stepLength ;
+  std::vector<double> stepTime ;
+  for ( std::vector<SDHCALHit*>::const_iterator it = hits.begin() ; it != hits.end() ; ++it )
+  {
+    stepCosAngle.push_back( std::cos( (*it)->getDeltaPos().angle( (*it)->getRPC()->rpcToGlobalCoordTransform( G4ThreeVector(0,0,1) ) ) ) ) ;
+    stepLength.push_back( (*it)->getTrueLength() ) ;
+    stepTime.push_back( (*it)->getTime() ) ;
+  }
+
+  rootWriter->setStepCosAngle( stepCosAngle ) ;
+  rootWriter->setStepLength( stepLength ) ;
+  rootWriter->setStepTime( stepTime ) ;
+  rootWriter->fillTree() ;
 }
